6750fb369e5d9d120b46ae573bdee286c568a201
Johnny Wong <johnny.wong2324@gmail>
2025-04-23 15:34:31 -0400

    okay actually finished day 1 of aoc

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+
+def pretty_print(part: int, result: Any):
+    print("######################################")
+    print("Part One answer:")
+    print(result)
+    print("######################################")
+
+    return
--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,19 @@
 import heapq
+from collections import Counter
+
+from typing import Any
 
 
-def main():
+def pretty_print(part: int, result: Any):
+    print("######################################")
+    print(f"Part {part} answer:")
+    print(result)
+    print("######################################")
+
+    return
+
+
+def read_input():
     left, right = [], []
 
     with open("input.txt", "r") as f:
@@ -11,16 +23,34 @@ def main():
             heapq.heappush(left, int(x[0]))
             heapq.heappush(right, int(x[1]))
 
+    return left, right
+
+
+def part_one():
+    left, right = read_input()
     total = 0
     while len(left) and len(right):
         l, r = heapq.heappop(left), heapq.heappop(right)
         difference = abs(l - r)
         total += difference
 
-    print(total)
+    pretty_print(1, total)
 
     return total
 
 
+def part_two():
+    left, right = read_input()
+
+    freq = Counter(right)
+
+    similarity_score = sum([l * freq.get(l, 0) for l in left])
+
+    pretty_print(2, similarity_score)
+
+    return similarity_score
+
+
 if __name__ == "__main__":
-    main()
+    part_one()
+    part_two()
